b9b1c940b85996e8358d16097d97ab3461587d5e
Sam Babiso <Sbabiso@gmail.com>
2019-10-21 10:43:13 -0400

    Initial Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { productListComponent } from './products/product-list.component';
+import { ConvertToSpacePipe } from './shared/convert-to-spaces.pipe';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    productListComponent,
+    ConvertToSpacePipe
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
